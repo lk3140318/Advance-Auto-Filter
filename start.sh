84bd950d9d83241ba72c4c8d64c90a296f0b8ef8
hellmava <146224731+hellmava@users.noreply.github.com>
2024-05-08 12:31:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone hellmava/MoviesX /MoviesX
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /MoviesX
 fi
-cd /Advance-Auto-Filter
+cd /MoviesX
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
